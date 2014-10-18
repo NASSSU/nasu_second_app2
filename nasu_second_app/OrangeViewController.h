1da231ee66a9a5e8c06c2c2b9d3d02918c3fad8a
usatie <SHUN@Shun-no-MacBook-Air.local>
2014-10-18 19:09:10 +0900

    made nasu button and usami button

---
+++ nasu_second_app/OrangeViewController.h
@@ -0,0 +1,14 @@
+//
+//  OrangeViewController.h
+//  nasu_second_app
+//
+//  Created by Shun Usami on 2014/10/18.
+//  Copyright (c) 2014年 nasuryota. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface OrangeViewController : UIViewController
+@property (strong, nonatomic) IBOutlet UIButton *button2;
+
+@end
